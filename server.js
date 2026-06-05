22d7c928722aabe3cd43ba823b671ce74a351ede
Prime <your@email.com>
2026-06-05 18:54:36 +0530

    fix: proxy /api to backend

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 const path = require('path')
 const app = express()
+
+app.use('/api', createProxyMiddleware({
+  target: process.env.VITE_API_URL,
+  changeOrigin: true,
+  pathRewrite: { '^/api': '' }
+}))
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
-app.listen(3001, () => console.log('Frontend running on port 3001'))
+app.listen(process.env.PORT || 3001, () => console.log('Frontend running'))
